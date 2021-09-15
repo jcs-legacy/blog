b99612a54bbd89eed93222342747745adf3ea5db
JenChieh <jcs090218@gmail.com>
2021-09-15 21:18:23 +0800

    Add scripts for utility use

---
+++ _scripts/build.bat
@@ -0,0 +1,13 @@
+@echo off
+:: ========================================================================
+:: $File: build.bat $
+:: $Date: 2021-09-15 21:07:55 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2021 by Shen, Jen-Chieh $
+:: ========================================================================
+
+cd ..
+
+hexo generate
---
+++ _scripts/run.bat
@@ -0,0 +1,13 @@
+@echo off
+:: ========================================================================
+:: $File: run.bat $
+:: $Date: 2021-09-15 21:04:17 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2021 by Shen, Jen-Chieh $
+:: ========================================================================
+
+cd ..
+
+hexo server
